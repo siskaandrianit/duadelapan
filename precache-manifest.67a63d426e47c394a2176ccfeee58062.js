b2b5631763e43fecb87cd0983a56d024043c1deb
siskaandrianit <siskaandriani1906@gmail.com>
2019-03-28 08:27:28 +0700

    Updates

---
+++ precache-manifest.67a63d426e47c394a2176ccfeee58062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/duadelapan/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c3edac1b7525cff22e0f",
+    "url": "/duadelapan/static/js/runtime~main.7eb5d352.js"
+  },
+  {
+    "revision": "0de47b3510ca2d5c0e4d",
+    "url": "/duadelapan/static/js/main.b38cfe6e.chunk.js"
+  },
+  {
+    "revision": "5711b84e88abdf95a81e",
+    "url": "/duadelapan/static/js/2.f4c73c90.chunk.js"
+  },
+  {
+    "revision": "0de47b3510ca2d5c0e4d",
+    "url": "/duadelapan/static/css/main.b1399cb4.chunk.css"
+  },
+  {
+    "revision": "1d990c159e3f366674b601938322db8a",
+    "url": "/duadelapan/index.html"
+  }
+];
