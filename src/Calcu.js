fb4d68a0bba00a717d8f99ebfd8967f9d9cf6241
siskaandrianit <siskaandriani1906@gmail.com>
2019-04-22 13:52:08 +0700

    satu

--- src/Calcu.js
+++ src/Calcu.js
@@ -3,15 +3,58 @@ import React, { Component } from 'react';
 class Calcu extends Component {
 	constructor(props) {
 		super(props);
-		
+		this.state = {
+			color: 'Tomat',
+			title:'Calcu',
+			angka1:0,
+			angka2:0,
+			operation :'plus',
+			
+		};
 	}
   render() {
     return (
 	
-	<img src="biru1.jpg" width="200"/>
+	<div>
+				<span className={"selected "+this.state.color}>
+			{this.state.title}
+				</span><br/><br/>
+				<input type="number" name="angka1" placeholder="angka 1" value={this.state.angka1} onChange={this.changehand.bind(this)}/>
+				<select name="operation" onChange={this.changehand.bind(this)}>
+				<option value="plus">+</option>
+				<option value="minus">-</option>
+				<option value="pow">*</option>
+				<option value="div">/</option>
+				</select>
+
+				   <input type="number" name="angka2" placeholder="angka 2"  value={this.state.angka2} onChange={this.changehand.bind(this)} />=
+        			 
+					
+        			<span> {this.hitung()}</span>
+					
+
+				
+			</div>
 			
     );
   }
+  changehand(event){
+
+		this.setState({
+			[event.target.name]:event.target.value,
+			
+		
+		});
+	}
+	hitung(){
+console.log(this.state.operation);
+	switch(this.state.operation){
+	case 'plus' :return parseInt(this.state.angka1) + parseInt(this.state.angka2);
+	case 'minus' :return parseInt(this.state.angka1) - parseInt(this.state.angka2);
+	case 'pow' :return parseInt(this.state.angka1) * parseInt(this.state.angka2);
+	case 'div':return parseInt(this.state.angka1) / parseInt(this.state.angka2);
+	}
+}
 }
 
 export default Calcu;
